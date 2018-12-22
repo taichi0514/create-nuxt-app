688ba631fef3209a8eb6a7bde1d326eebad8b9c2
taichimurai <murai@tam-bourine.co.jp>
2018-12-22 18:47:11 +0900

    config調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,26 +2,26 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'universal',
-
+  srcDir: 'app',
   /*
   ** Headers of the page
   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: pkg.description}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
 
   /*
   ** Global CSS
@@ -59,7 +59,7 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      
+
     }
   }
 }
